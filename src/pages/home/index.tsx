930c2e76d949941a70c383e7531e8b287349302a
jakovid <jacobk.dowdle@gmail.com>
2023-01-04 23:27:36 +0800

    began to create profile card component and style sheet

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,11 +2,13 @@ import Post from "../../components/home/post";
 import "./index.css";
 import Header from "../../components/header";
 import News from "../../components/home/news";
+import ProfileCard from "../../components/home/profile-card";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <ProfileCard />
       <Post />
       <News />
 
