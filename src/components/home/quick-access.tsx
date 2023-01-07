1bafd94277a3018e4edc73694690dfbb2cc47453
jakovid <jacobk.dowdle@gmail.com>
2023-01-07 13:05:39 +0800

    began creating the quick access component

---
+++ src/components/home/quick-access.tsx
@@ -0,0 +1,23 @@
+
+
+export default function QuickAccess(){
+    return(
+        <div className="quick-access-cont">
+            <div className="quick-access-trending-cont">
+                <div className="quick-access-title">Trending Now</div>
+            </div>
+
+            <div className="quick-access-groups-cont">
+                <div className="quick-access-title">Groups</div>
+            </div>
+
+            <div className="quick-access-events-cont">
+                <div className="quick-access-title">Upcoming Events</div>
+            </div>
+
+            <div className="quick-access-hashtags-cont">
+                <div className="quick-access-title">My Hashtags</div>
+            </div>
+        </div>
+    )
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,12 +4,14 @@ import Header from "../../components/header";
 import News from "../../components/home/news";
 import Advertisement from "../../components/home/advertisement";
 import ProfileCard from "../../components/home/profile-card";
+import QuickAccess from "../../components/home/quick-access";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <ProfileCard />
+      <QuickAccess />
       <Post />
       <News />
       <Advertisement />
