9dde27f7b0ebe7e9b065b3c030bddeb0e874ee7c
jakovid <jacobk.dowdle@gmail.com>
2023-01-08 15:56:22 +0800

    quick access automatically populates now

--- src/components/home/quick-access.tsx
+++ src/components/home/quick-access.tsx
@@ -5,26 +5,9 @@ import '../../styles/quick-access.css';
 
 const profileCardData = {
     userId: 123,
-    groups: [{groupId: 2, groupName:'Open Mic Support Group', groupURL: '/openmicsupportgroup'}],
-    events: [{eventId: 1, eventName:'Sad Mic at Restaurant', eventURL: '/sadmic'}],
-    hashtags: [{hashtagId: 9, hashtagName:'comedy', hashtagUrl:'/comedy'}]
-}
-
-function PopulateQuickAccessItems( QuickAccessItems: string){
-    return(
-        <div></div>
-    )
-}
-
-function CreateQuickAccessItem( itemType: string, itemName: string, itemLink: string ) {
-    return(
-        <div className='quick-access-item-cont'>
-            <a href={itemLink}>
-                <img className='quick-access-item-icon' src={itemType} />
-                <div className='quick-access-item'>{itemName}</div>
-            </a>
-        </div>
-    )
+    groups: [{id: 2, name:'Open Mic Support Group', url: '/groups/openmicsupportgroup'}],
+    events: [{id: 1, name:'Sad Mic at Restaurant', url: '/events/sadmic'}],
+    hashtags: [{id: 9, name:'comedy', url:'/hashtags/comedy'}]
 }
 
 export default function QuickAccess(){
@@ -40,25 +23,43 @@ export default function QuickAccess(){
 
             <div className="quick-access-category-cont">
                 <div className="quick-access-title">Groups</div>
-                <div className="quick-access-item-cont">
-                    <img className="quick-access-item-icon" src={Group}/>
-                    <div className="quick-access-item">Open Mic Support Group</div>
+                <div>
+                {profileCardData.groups.map(item => {
+                        return(
+                            <div className="quick-access-item-cont">
+                                <img className="quick-access-item-icon" src={Group}/>
+                                <div className="quick-access-item"><a href={item.url}>{item.name}</a></div>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
 
             <div className="quick-access-category-cont">
                 <div className="quick-access-title">Upcoming Events</div>
-                <div className="quick-access-item-cont">
-                    <img className="quick-access-item-icon" src={Event}/>
-                    <div className="quick-access-item">Sad Mic at Restaurant</div>
+                <div>
+                {profileCardData.events.map(item => {
+                        return(
+                            <div className="quick-access-item-cont">
+                                <img className="quick-access-item-icon" src={Event}/>
+                                <div className="quick-access-item"><a href={item.url}>{item.name}</a></div>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
 
             <div className="quick-access-category-cont">
                 <div className="quick-access-title">My Hashtags</div>
-                <div className="quick-access-item-cont">
-                    <img className="quick-access-item-icon" src={Hashtag}/>
-                    <div className="quick-access-item">Comedy</div>
+                <div>
+                {profileCardData.hashtags.map(item => {
+                        return(
+                            <div className="quick-access-item-cont">
+                                <img className="quick-access-item-icon" src={Hashtag}/>
+                                <div className="quick-access-item"><a href={item.url}>{item.name}</a></div>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         </div>
