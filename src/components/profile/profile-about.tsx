82dec63d2f02af56b9f947ab14786e9dc02343be
jakovid <jacobk.dowdle@gmail.com>
2023-01-14 21:57:19 +0800

    created about section and added about info to the fake user

---
+++ src/components/profile/profile-about.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { FakeUserData } from "../../dummy-data/fake-users"
+import "../../styles/profile-about.css";
+
+export default function ProfileAbout() {
+
+  return(
+    <div className="profile-about-cont">
+      <div className="profile-about-title">About</div>
+      <div className="profile-about-text">{FakeUserData.about}</div>
+    </div>
+  )
+}
