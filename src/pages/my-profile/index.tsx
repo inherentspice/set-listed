72e93d5c85d473e4c5f5335959e5be90df97dbd8
Stephen Smith <stephenlehanesmith@gmail.com>
2023-01-15 17:37:53 +0800

    created index.css for profile and moved common components from home index.css to app.css

--- src/pages/my-profile/index.tsx
+++ src/pages/my-profile/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ProfileHero from "../../components/profile/profile-hero";
 import ProfileAbout from "../../components/profile/profile-about";
+import Featured from "../../components/profile/featured";
 
 export default function MyProfile() {
   return (
-    <div>
+    <div className="page-cont">
       <ProfileHero />
       <ProfileAbout />
+      <Featured />
     </div>
   );
 }
