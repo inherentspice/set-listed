b34ea1cae9e66c6be795045cd55c3eb78fd8e997
jakovid <jacobk.dowdle@gmail.com>
2023-01-15 22:05:39 +0800

    added edit button for future use

--- src/components/profile/profile-about.tsx
+++ src/components/profile/profile-about.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { FakeUserData } from "../../dummy-data/fake-users";
 import "../../styles/profile-about.css";
+import Edit from "../../media/icons/edit.png";
 
 export default function ProfileAbout() {
 
   return(
     <div className="profile-about-cont comp">
-      <div className="profile-about-title">About</div>
+      <div className="profile-about-title-cont">
+        <div className="profile-about-title">About</div>
+        <img className="profile-about-edit" src={Edit} />
+      </div>
       <div className="profile-about-text">{FakeUserData.about}</div>
     </div>
   );
