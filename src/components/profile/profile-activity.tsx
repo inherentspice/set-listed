5bfd721c970c2ab867281b06e813284fa9305a4b
jakovid <jacobk.dowdle@gmail.com>
2023-01-17 16:47:04 +0800

    began to create a mock activity post

--- src/components/profile/profile-activity.tsx
+++ src/components/profile/profile-activity.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "../../styles/profile-activity.css";
+import Like from "../../media/icons/like.png";
+import BillBurr from "../../media/profile/billburr.png";
 
 export default function ProfileActivity() {
     return(
@@ -14,7 +16,28 @@ export default function ProfileActivity() {
                 </div>
             </div>
             <div className="profile-activity-posts-cont">
-
+                <div className="profile-activity-post-item">
+                    <div className="profile-activity-post-info">
+                        <div className="profile-activity-post-author">Ben Dover posted this</div>
+                        <div className="profile-activity-post-time">24h</div>
+                    </div>
+                    <div className="profile-activity-post-first-sentence">I truly believe that I am the Bill Burr of the improv stage.</div>
+                    <div className="profile-activity-post-show-more">...show more</div>
+                    <div className="profile-activity-post">
+                        <img className="profile-activity-post-img" src={BillBurr} />
+                        <div className="profile-activity-post-right">
+                            <div className="profile-activity-post-title"></div>
+                            <div className="profile-activity-post-about">
+                                <div className="profile-activity-post-url">comedy.com</div>
+                                <div className="profile-activity-post-read-time">2 min read</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="profile-activity-post-likes">
+                        <img className="profile-activity-post-like-img" src={Like} />
+                        <div className="profile-activity-post-like-count">1</div>
+                    </div>
+                </div>
             </div>
             <div className="profile-activity-show-all">Show all activity</div>
         </div>
