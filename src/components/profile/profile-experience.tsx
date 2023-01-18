0ef4a2143bcb182849f783a54c542ee8891b5c94
jakovid <jacobk.dowdle@gmail.com>
2023-01-18 13:40:28 +0800

    began to create the experience component

---
+++ src/components/profile/profile-experience.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Edit from "../../media/icons/edit.png";
+import Add from "../../media/icons/add.png";
+
+export default function ProfileExperience(){
+    return(
+        <div className="profile-experience-cont comp">
+            <div className="profile-experience-header">
+                <div className="profile-experience-header-title">Experience</div>
+                <div className="profile-experience-header-buttons">
+                    <img className="profile-experience-header-btn" src={Add} />
+                    <img className="profile-experience-header-btn" src={Edit} />
+                </div>
+            </div>
+            <div className="profile-experience-items-cont">
+                <div className="profile-experience-item">
+                    <img className="profile-experience-item-img" />
+                    <div className="profile-experience-item-info-cont">
+                        <div className="profile-experience-item-title">Improv Host</div>
+                        <div className="profile-experience-item-venue">Danny's Dive and Beer Shack</div>
+                        <div className="profile-experience-item-duration-cont">
+                            <div className="profile-experience-item-dates">Jan 7, 1989 - Present</div>
+                            <div>|</div>
+                            <div className="profile-experience-item-duration">24 Years</div>
+                        </div>
+                        <div className="profile-experience-item-location">Bangkok, Thailand</div>
+                        <div className="profile-experience-item-description">I run a show every night.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
