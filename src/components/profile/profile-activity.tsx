8c845fa265cab274305582b7a82f126c548ff29a
jakovid <jacobk.dowdle@gmail.com>
2023-01-18 13:12:03 +0800

    created new activity posts and automated activity item creation

--- src/components/profile/profile-activity.tsx
+++ src/components/profile/profile-activity.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import "../../styles/profile-activity.css";
 import Like from "../../media/icons/like.png";
 import Dislike from "../../media/icons/dislike.png";
-import BillBurr from "../../media/profile/billburr.png";
+import { FakeUserData } from "../../dummy-data/fake-users";
+import convertDate from "../../utilities/convert-date";
+
 
 export default function ProfileActivity() {
     return(
@@ -10,37 +12,42 @@ export default function ProfileActivity() {
             <div className="profile-activity-header">
                 <div className="profile-activity-header-left">
                     <div className="profile-activity-header-title">Activity</div>
-                    <div className="profile-activity-header-followers">5,865 followers</div>
+                    <div className="profile-activity-header-followers">{FakeUserData.userFollwers+" followers"}</div>
                 </div>
                 <div className="profile-activity-header-right">
                     <div className="profile-activity-start-post">Start a Post</div>
                 </div>
             </div>
             <div className="profile-activity-posts-cont">
-                <div className="profile-activity-post-item">
-                    <div className="profile-activity-post-info">
-                        <div className="profile-activity-post-author">Ben Dover</div>
-                        <div className="profile-activity-post-time">posted this | 24h</div>
-                    </div>
-                    <div className="profile-activity-post-description">I truly believe that I am the Bill Burr of the improv stage.</div>
-                    <div className="profile-activity-post">
-                        <img className="profile-activity-post-img" src={BillBurr} />
-                        <div className="profile-activity-post-right">
-                            <div className="profile-activity-post-title">10 Reasons I am Bill Burr</div>
-                            <div className="profile-activity-post-about">
-                                <div className="profile-activity-post-url">bendover.net</div>
-                                <div>|</div>
-                                <div className="profile-activity-post-read-time">2 min read</div>
+                {FakeUserData.activity.map(item => {
+                    return(
+                        <div className="profile-activity-post-item">
+                        <div className="profile-activity-post-info">
+                            <div className="profile-activity-post-author">{FakeUserData.userFirstName+" "+FakeUserData.userLastName}</div>
+                            <div className="profile-activity-post-time">{"posted this | "+convertDate(item.date)}</div>
+                        </div>
+                        <div className="profile-activity-post-description">{item.description}</div>
+                        <div className="profile-activity-post">
+                            <img className="profile-activity-post-img" src={item.img} />
+                            <div className="profile-activity-post-right">
+                                <div className="profile-activity-post-title">{item.title}</div>
+                                <div className="profile-activity-post-about">
+                                    <div className="profile-activity-post-url">{item.url}</div>
+                                    <div>|</div>
+                                    <div className="profile-activity-post-read-time">{item.read+" read"}</div>
+                                </div>
                             </div>
                         </div>
+                        <div className="profile-activity-post-likes">
+                            <img className="profile-activity-post-like-img" src={Like} />
+                            <div className="profile-activity-post-like-count">{item.likes}</div>
+                            <img className="profile-activity-post-like-img" src={Dislike} />
+                            <div className="profile-activity-post-like-count">{item.dislikes}</div>
+                        </div>
                     </div>
-                    <div className="profile-activity-post-likes">
-                        <img className="profile-activity-post-like-img" src={Like} />
-                        <div className="profile-activity-post-like-count">1</div>
-                        <img className="profile-activity-post-like-img" src={Dislike} />
-                        <div className="profile-activity-post-like-count">1035</div>
-                    </div>
-                </div>
+    
+                    )
+                })}
             </div>
             <div className="profile-activity-show-all">Show all activity</div>
         </div>
--- src/dummy-data/fake-users.tsx
+++ src/dummy-data/fake-users.tsx
@@ -3,6 +3,8 @@ import BenDoverBG from "../media/home/profile-background.jpg";
 import tiktok from "../media/icons/tik-tok.png";
 import instagram from "../media/icons/instagram.png";
 import youtube from "../media/icons/youtube.png";
+import BillBurr from "../media/profile/billburr.png";
+import SteveHarvey from "../media/profile/steveharvey.png";
 
 export const FakeUserData = {
   userFirstName: "Ben",
@@ -17,9 +19,11 @@ export const FakeUserData = {
   userTagline: "The show didn't get canceled, I quit.",
   userProfileViews: 1,
   userPostImpressions: 69,
+  userFollwers: 1234,
   groups: [{id: 1, name:"Open Mic Support Group", url: "/groups/openmicsupportgroup"}],
   events: [{id: 2, name:"Sad Mic at Restaurant", url: "/events/sadmic"}],
   hashtags: [{id: 3, name:"comedy", url:"/hashtags/comedy"}],
   socialMedia: [{id: 4, socialMedia: "instagram", img: instagram, username: "@bendover69", followers: 3}, { id: 5, socialMedia: "youtube", img: youtube, username: "@BenDover69", followers: 14}, { id: 6, socialMedia: "tiktok", img: tiktok, username: "@BenChiling", followers: 100000}],
-  about:"I just received the initial deposit for a corporate show in Dubai that's coming up in a couple of weeks. The show pays $8000, yes, that's right, EIGHT THOUSAND US DOLLARS for less than one hour's work! And yes, I'll be flying in First Class as usual, in unbelievable luxury, with a flat bed!  I'll be checking out the indoor ski range, surfing on the sand dunes, visiting the tallest building in the world and the world's largest shopping mall, and luxuriating by the pool at my five star hotel. And yes, I will send pictures!"
+  about:"I just received the initial deposit for a corporate show in Dubai that's coming up in a couple of weeks. The show pays $8000, yes, that's right, EIGHT THOUSAND US DOLLARS for less than one hour's work! And yes, I'll be flying in First Class as usual, in unbelievable luxury, with a flat bed!  I'll be checking out the indoor ski range, surfing on the sand dunes, visiting the tallest building in the world and the world's largest shopping mall, and luxuriating by the pool at my five star hotel. And yes, I will send pictures!",
+  activity: [{id: 7, date: new Date("January 1, 2023"), description: "I truly believe that I am the Bill Burr of the improv stage", title: "10 Reasons I am Bill Burr", url: "bendover.net", read: "2 min", img: BillBurr, likes: 1, dislikes: 1035}, {id: 8, date: new Date("December 25, 2022"), description: "Steve Harvey's suits are fake and I have the proof they all zip up in the back.", title: "Steve Harvey the Liar", url: "bendover.net", read: "24 min", img: SteveHarvey, likes: 2560, dislikes: 1}]
 };
