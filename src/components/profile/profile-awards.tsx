3ad16b7964e0700dbd8533d3950254c8263452d3
Stephen Smith <stephenlehanesmith@gmail.com>
2023-01-21 13:46:31 +0800

    added basic awards component

---
+++ src/components/profile/profile-awards.tsx
@@ -0,0 +1,26 @@
+import Add from "../../media/icons/add.png";
+import Edit from "../../media/icons/edit.png";
+import { FakeUserData } from "../../dummy-data/fake-users";
+
+export default function ProfileAwards() {
+  return (
+    <div className="featured-cont comp">
+      <div className="featured-header">
+        <h2>Awards & Achievements</h2>
+        <div className="profile-experience-header-buttons">
+          <img className="profile-experience-header-btn" src={Add} />
+          <img className="profile-experience-header-btn" src={Edit} />
+        </div>
+      </div>
+      <div className="awards-cont">
+        {FakeUserData.awards.map(award => {
+          return (
+          <div className="award-cont" key={award.id}>
+            <h4>{award.title}</h4>
+          </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
