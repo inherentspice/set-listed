28b4db2a7d800973b464b16c189c4822db046e31
Stephen Smith <stephenlehanesmith@gmail.com>
2023-01-24 14:41:05 +0800

    cleaned up code in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,11 @@
-// import React, { useEffect, useState } from "react";
-// import Home from "./pages/home/index";
-// import Login from "./pages/login";
-// import AuthService from "./services/home/auth";
-// import "./App.css";
+import React from "react";
+import Home from "./pages/home/index";
+import "./App.css";
 
-// function App() {
+function App() {
+  return (
+    <Home />
+  );
+}
 
-//   const [user, setUser] = useState<string>("");
-
-//   useEffect(() => {
-//     AuthService.checkSession()
-//       .then((result) => {
-//         setUser(result.data.id);
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//         setUser("");
-//       });
-//   }, []);
-
-//   console.log(user);
-
-//   return (
-//     <>
-//       {!user && <Login />}
-//       {user && <Home />}
-//     </>
-//   );
-// }
-
-// export default App;
-
-export {};
+export default App;
