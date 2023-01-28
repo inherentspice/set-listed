65a75f82791ba60d4cc9a47148283b02b92a3d17
Jacob Dowdle <jacobk.dowdle@gmail.com>
2023-01-28 16:41:08 +0800

    edit experience now populates the form with current data, but unable to load start and end date data for now

--- src/components/profile/profile-experience.tsx
+++ src/components/profile/profile-experience.tsx
@@ -12,7 +12,7 @@ import CancelButton from "../../media/icons/cancel.png";
 export default function ProfileExperience(){
     const [expandedAddExperience, setExpandedAddExperience] = useState<null | number>(null);
     const [expandedEditExperience, setExpandedEditExperience] = useState<null | number>(null);
-
+    const [experienceIndex, setExperienceIndex] = useState(-1);
 
     const { username } = useParams();
     const userIndex = FakeUserData.findIndex(x => x.username === username);
@@ -20,8 +20,9 @@ export default function ProfileExperience(){
     function handleAddExperienceClick(id: number): void{
         setExpandedAddExperience(id);
     }
-    function handleEditExperienceClick(id: number): void{
+    function handleEditExperienceClick(id: number, index: number): void{
         setExpandedEditExperience(id);
+        setExperienceIndex(index);
         console.log("click");
     }
 
@@ -41,32 +42,31 @@ export default function ProfileExperience(){
                     <h2 className="expanded-profile-overlay-title">Add Your Experience</h2>
                     <img className="start-post-cancel" src={CancelButton} onClick={() => handleAddExerienceClose()} />
                 </div>
-                <form>
+                <form className="add-experience-form">
                 <div className="add-experience-form-item">
                     <label>Title:</label>
-                    <input></input>
+                    <input placeholder="Ex: Open Mic Host"></input>
                 </div>
                 <div className="add-experience-form-item">
                     <label>Venue:</label>
-                    <input></input>
+                    <input placeholder="Ex: Tai Chi Comedy"></input>
                 </div>
                 <div className="add-experience-form-item">
                     <label>Start:</label>
-                    <input></input>
+                    <input placeholder="DD/MM/YYYY" type="date"></input>
                 </div>
                 <div className="add-experience-form-item">
                     <label>End:</label>
-                    <input/>
-                    <label>On-Going</label>
-                    <input type="radio"/>
+                    <input placeholder="DD/MM/YYYY - leave blank if you still perform here" type="date"/>
+                    
                 </div>
                 <div className="add-experience-form-item">
                     <label>Location:</label>
-                    <input></input>
+                    <input placeholder="City, Country"></input>
                 </div>
                 <div className="add-experience-form-item">
                     <label>Description:</label>
-                    <input></input>
+                    <input placeholder="Ex: I host a monthly Drag Show Brunch."></input>
                 </div>
                 <div className="edit-profile-hero-submit">
                     <button className="edit-profile-hero-submit-btn" type="submit">Save</button>
@@ -81,12 +81,43 @@ export default function ProfileExperience(){
     function ShowEditExperience() {
         return ReactDOM.createPortal(
           <>
-            <div className="expanded-start-post-cont" key={FakeUserData[userIndex].id}></div>
-            <div className="expanded-start-post">
+            <div className="expanded-profile-overlay-cont" key={FakeUserData[userIndex].id}></div>
+            <div className="expanded-profile-overlay">
                 <div className="expanded-profile-overlay-header-cont">
                     <h2 className="expanded-profile-overlay-title">Edit Your Experience</h2>
                     <img className="start-post-cancel" src={CancelButton} onClick={() => handleEditExerienceClose()} />
                 </div>
+                <form className="add-experience-form">
+                <div className="add-experience-form-item">
+                    <label>Title:</label>
+                    <input defaultValue={FakeUserData[userIndex].experience[experienceIndex].title}></input>
+                </div>
+                <div className="add-experience-form-item">
+                    <label>Venue:</label>
+                    <input defaultValue={FakeUserData[userIndex].experience[experienceIndex].venue}></input>
+                </div>
+                <div className="add-experience-form-item">
+                    <label>Start:</label>
+                    <input type="date"></input>
+                </div>
+                <div className="add-experience-form-item">
+                    <label>End:</label>
+                    <input type="date"/>
+                    
+                </div>
+                <div className="add-experience-form-item">
+                    <label>Location:</label>
+                    <input defaultValue={FakeUserData[userIndex].experience[experienceIndex].location}></input>
+                </div>
+                <div className="add-experience-form-item">
+                    <label>Description:</label>
+                    <input defaultValue={FakeUserData[userIndex].experience[experienceIndex].description}></input>
+                </div>
+                <div className="edit-profile-hero-submit">
+                    <button className="edit-profile-hero-submit-btn" type="submit">Save</button>
+                </div>
+            </form>
+
             </div>
           </>,
           document.body
@@ -99,7 +130,6 @@ export default function ProfileExperience(){
                 <div className="profile-experience-header-title">Experience</div>
                 <div className="profile-experience-header-buttons">
                     <img className="profile-experience-header-btn" src={Add} onClick={() => handleAddExperienceClick(FakeUserData[userIndex].id)} />
-                    <img className="profile-experience-header-btn" src={Edit} onClick={() => handleEditExperienceClick(FakeUserData[userIndex].id)} />
                 </div>
             </div>
             <div className="profile-experience-items-cont">
@@ -108,8 +138,15 @@ export default function ProfileExperience(){
                         <div className="profile-experience-item">
                             <img className="profile-experience-item-img" src={item.img} />
                             <div className="profile-experience-item-info-cont">
-                                <div className="profile-experience-item-title">{item.title}</div>
-                                <div className="profile-experience-item-venue">{item.venue}</div>
+                                <div className="profile-experience-item-head">
+                                    <div className="profile-experience-head-left">
+                                        <div className="profile-experience-item-title">{item.title}</div>
+                                        <div className="profile-experience-item-venue">{item.venue}</div>
+                                    </div>
+                                    <div className="profile-experience-item-head-right">
+                                        <img className="profile-experience-header-btn" src={Edit} onClick={() => handleEditExperienceClick(FakeUserData[userIndex].id, FakeUserData[userIndex].experience.indexOf(item))} />
+                                    </div>
+                                </div>                                    
                                 <div className="profile-experience-item-duration-cont">
                                     <div className="profile-experience-item-dates">{item.start.toString().slice(4,16) +" - "+ (item.end.toString().slice(4,16) === new Date(Date.now()).toString().slice(4,16) ? "Present" : item.end.toString().slice(4,16))}</div>
                                     <div>|</div>
