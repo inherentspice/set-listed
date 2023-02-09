d847c84753590dd0b8ef8a872191003731a7de0f
Stephen Smith <stephenlehanesmith@gmail.com>
2023-02-09 06:24:15 +0100

    created interfaces for profile data

--- src/services/home/profile.tsx
+++ src/services/home/profile.tsx
@@ -1,20 +1,21 @@
 import axios from "axios";
 import { AxiosResponse } from "axios";
+import Profile, {ProfileCardData} from "../../types/profile";
 
 const baseUrl = "/profilecard";
 
-interface ProfileCardData {
-  data: object,
-  url: string,
-  status: number,
-}
 
 const getOne = (id: string): Promise<AxiosResponse<ProfileCardData>> => {
   return axios.get(`${baseUrl}/${id}`);
 };
 
+const getProfile = (id: string): Promise<AxiosResponse<Profile>> => {
+  return axios.get(`/profile/${id}`);
+};
+
 const ProfileCardService = {
   getOne,
+  getProfile
 };
 
 export default ProfileCardService;
---
+++ src/types/profile.tsx
@@ -0,0 +1,72 @@
+export interface AboutData {
+  user: string,
+  id: string,
+  content: string
+}
+
+export interface AwardData {
+  user: string,
+  id: string,
+  content: string
+}
+
+export interface ExperienceData {
+  content: string,
+  id: string,
+  user: string,
+  venue: string
+}
+
+export interface FeaturedData {
+  user: string,
+  image: string,
+  cloudinaryId: string,
+  title: string,
+  content: string,
+  id: string
+}
+
+export interface PostData {
+  content: string,
+  likes: number,
+  user: string,
+  createdAt: Date,
+  id: string
+}
+
+export interface ProfileCardData {
+  firstName: string,
+  lastName: string,
+  user: string,
+  country: string,
+  city: string,
+  id: string,
+  image: string,
+  socials: string[],
+  tagline: string,
+  backgroundImage: string,
+  backgroundCloudinaryId: string,
+  userPostImpressions: number,
+  userProfileViews: number,
+}
+
+export interface SkillData {
+  user: string,
+  content: string,
+  endorsments: number,
+  id: string,
+}
+
+export interface ProfileData {
+  about: AboutData,
+  award: AwardData,
+  experience: ExperienceData,
+  featured: FeaturedData,
+  post: PostData,
+  profileCard: ProfileCardData,
+  skill: SkillData
+}
+
+export default interface Profile {
+  data: ProfileData,
+};
