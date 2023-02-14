c42522d17caf761349c38d8948065cab435e3d8a
jakovid <jacobk.dowdle@gmail.com>
2023-02-14 20:55:36 +0800

    began to create and style the invitations comp

---
+++ src/components/network/invitations.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Denise from "../../media/home/denise-profile-picture.png";
+import Equal from "../../media/icons/equal.png";
+import "../../styles/my-network/invitations.css";
+
+export default function NetworkInvitations() {
+    return(
+        <div className="network-invitations-cont comp">
+            <div className="network-invitation-header">
+                <h2>Invitations</h2>
+                <a>See All</a>
+            </div>
+            <div className="network-invitation-items-cont">
+
+                <div className="network-invitation-item">
+                    <img className="profile-picture-medium" src={ Denise } />
+                    <div className="network-invitation-info">
+                        <div className="network-invitation-name">Denis Ferguson</div>
+                        <div className="network-invitation-tagline">Improv | Storytelling</div>
+                        <div className="network-invitation-shared-connections">
+                            <img src={Equal} className="network-invitation-shared-connections-img" />
+                            <div className="network-invitation-shared-connections-people">Stevie Smiff & Jacob Tones</div>
+                        </div>
+                    </div>
+                    <div className="network-invitation-btns">
+                        <button className="network-invitation-ignore-btn">Ignore</button>
+                        <button className="network-invitation-accept-btn">Accept</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
