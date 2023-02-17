061585f5132c73061139c8169b9cae4f554172f0
jakovid <jacobk.dowdle@gmail.com>
2023-02-17 23:37:32 +0800

    began to create recommended comp for my network page

---
+++ src/components/network/recommended.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NetworkRecommended() {
+    return(
+        <div className="network-recommended-cont comp">
+            {/* this will be filled with comps */}
+        </div>
+    )
+}
--- src/pages/my-network/index.tsx
+++ src/pages/my-network/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NetworkManage from "../../components/network/manage-network";
 import NetworkInvitations from "../../components/network/invitations";
+import NetworkRecommended from "../../components/network/recommended";
 
 export default function MyNetwork() {
   return (
     <div>
       <NetworkManage />
       <NetworkInvitations />
+      <NetworkRecommended />
     </div>
   );
 }
