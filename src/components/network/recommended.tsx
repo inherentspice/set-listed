42f857916cc6911066c63a72da8218371c6b2b1c
Jacob Dowdle <jacobk.dowdle@gmail.com>
2023-02-19 11:33:14 +0800

    updated recommended card to be able to replicate and updated styling for imgs

--- src/components/network/recommended.tsx
+++ src/components/network/recommended.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import FollowRecommendation from "./follow-recommendation";
 import "../../styles/my-network/recommended.css";
+import { RecommendedData } from "../../dummy-data/recommended";
 
 export default function NetworkRecommended() {
     return(
         <div className="network-recommended-cont comp">
             {/* this will be filled with comps */}
-            <FollowRecommendation />
+            <FollowRecommendation  cardData={RecommendedData[0]} cardTitle="Performers you follow also follow these performers" />
+            <FollowRecommendation  cardData={RecommendedData[1]} cardTitle="Venues you might enjoy" />
+
         </div>
     )
 }
