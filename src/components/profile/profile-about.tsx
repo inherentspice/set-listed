805feccf70e2e2ba88090b8ca474f106d545b7d5
Jacob Dowdle <jacobk.dowdle@gmail.com>
2023-02-27 15:18:05 +0800

    minor analytics style update

--- src/components/profile/profile-about.tsx
+++ src/components/profile/profile-about.tsx
@@ -93,7 +93,7 @@ export default function ProfileAbout(props: {about: AboutData[], userProfile: bo
   return(
     <div className="profile-about-cont comp">
       <div className="profile-about-title-cont">
-        <div className="profile-about-title">About</div>
+        <h2 className="profile-about-title">About</h2>
 
         {props.userProfile && <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48" className="profile-about-edit" onClick={() => handleEditAboutClick(about.id)}><path d="M180 876h44l443-443-44-44-443 443v44Zm614-486L666 262l42-42q17-17 42-17t42 17l44 44q17 17 17 42t-17 42l-42 42Zm-42 42L248 936H120V808l504-504 128 128Zm-107-21-22-22 44 44-22-22Z" /></svg>}
       </div>
