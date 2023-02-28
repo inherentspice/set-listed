52bfd1b733d77a716794cce0f68f4e2d1af8ad53
Jacob Dowdle <jacobk.dowdle@gmail.com>
2023-02-28 15:37:02 +0800

    updated profile activity style

--- src/components/profile/profile-about.tsx
+++ src/components/profile/profile-about.tsx
@@ -97,8 +97,10 @@ export default function ProfileAbout(props: {about: AboutData[], userProfile: bo
 
         {props.userProfile && <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48" className="profile-about-edit" onClick={() => handleEditAboutClick(about.id)}><path d="M180 876h44l443-443-44-44-443 443v44Zm614-486L666 262l42-42q17-17 42-17t42 17l44 44q17 17 17 42t-17 42l-42 42Zm-42 42L248 936H120V808l504-504 128 128Zm-107-21-22-22 44 44-22-22Z" /></svg>}
       </div>
-      {about.content.length <=0 ? <p>Add a short bio about yourself by clicking the edit bio button above!</p> : <div className="profile-about-text">{about.content}</div> }
-      {expandedEditAbout && <ShowEditProfileAbout />}
+      <div className="profile-about-text-cont">
+        {about.content.length <=0 ? <p>Add a short bio about yourself by clicking the edit bio button above!</p> : <div className="profile-about-text">{about.content}</div> }
+        {expandedEditAbout && <ShowEditProfileAbout />}
+      </div>
     </div>
   );
 }
--- src/components/profile/profile-activity.tsx
+++ src/components/profile/profile-activity.tsx
@@ -271,7 +271,7 @@ export default function ProfileActivity(props: {profileCard: ProfileCardData[], 
             <div className="profile-activity-header-followers">{profileCard.userProfileViews+" followers"}</div>
           </div>
           <div className="profile-activity-header-right">
-            {props.userProfile && <div className="profile-activity-start-post" onClick={() => handleStartPostClick(profileCard.id)}>Start a Post</div>}
+            {props.userProfile && <div className="secondary-button" onClick={() => handleStartPostClick(profileCard.id)}>Start a Post</div>}
           </div>
         </div>
         <div className="profile-activity-posts-cont">
@@ -284,7 +284,7 @@ export default function ProfileActivity(props: {profileCard: ProfileCardData[], 
                 </div>
                 <p className="profile-activity-post-description">{item.content}</p>
                 <div className="profile-activity-post-likes">
-                  <img className="profile-activity-post-like-img" onClick={() => handleLikePostClick(item.id, props.viewingUser)} src={Like} />
+                  <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48" className="profile-activity-post-like-img" onClick={() => handleLikePostClick(item.id, props.viewingUser)}><path d="M716 936H272V424l278-288 39 31q6 5 9 14t3 22v10l-45 211h299q24 0 42 18t18 42v81.839q0 7.161 1.5 14.661T915 595L789 885q-8.878 21.25-29.595 36.125Q738.689 936 716 936Zm-384-60h397l126-299v-93H482l53-249-203 214v427Zm0-427v427-427Zm-60-25v60H139v392h133v60H79V424h193Z"/></svg>
                   <div className="profile-activity-post-like-count">{item.likes ? item.likes.length : 0}</div>
                   {props.userProfile && <button className="post-edit" onClick={() => handleEditPostClick(item.id)}>edit</button>}
                   {props.userProfile && <button className="post-edit" onClick={(e) => handleDeletePostClick(e, item.id)}>delete</button>}
@@ -293,7 +293,7 @@ export default function ProfileActivity(props: {profileCard: ProfileCardData[], 
               );
             })}
         </div>
-        <div className="profile-activity-show-all">Show all activity</div>
+        <div className="profile-activity-show-all">Show All Activity</div>
         {expandedStartPost && <ShowStartPost />}
         {expandedEditPost && <ShowEditPost />}
         {err && <ErrorMessage/>}
