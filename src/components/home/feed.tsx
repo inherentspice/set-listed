394bf1a00cb4daf979b76e02e1823dd46d689b1f
Jacob Dowdle <jacobk.dowdle@gmail.com>
2023-03-01 10:35:22 +0800

    i changed up the home page feed for some reason.

--- src/components/home/feed.tsx
+++ src/components/home/feed.tsx
@@ -5,6 +5,7 @@ import { PostData, ProfileCardData } from "../../types/profile";
 import convertDate from "../../utilities/convert-date";
 import Like from "../../media/icons/like.png";
 import "../../styles/home/feed.css";
+import ChatBubble from "../../media/icons/chat-bubble.png";
 
 
 export default function Feed(props: {post: PostData, viewingUser: string}) {
@@ -46,23 +47,32 @@ export default function Feed(props: {post: PostData, viewingUser: string}) {
 
   return (
     <div className="home-feed-cont comp">
-      <div className="profile-info-cont">
+      <div className="home-feed-left">
+        <div className="profile-activity-post-likes">
+          <img className="profile-activity-post-like-img" onClick={() => handleLikePostClick(props.post.id, props.viewingUser)} src={Like} />
+          <div className="profile-activity-post-like-count">{props.post.likes ? props.post.likes.length : 0}</div>
+        </div>
         <a href={`/user/${props.post.user}`}>
-        {profile?.image ?
-          <img src={profile.image} alt="" className="profile-picture-small"/>
-          : <></>}
+          {profile?.image ?
+            <img src={profile.image} alt="" className="profile-picture-medium"/>
+            : <></>}
         </a>
-        <div className="profile-text-info-cont">
-          <p className="profile-name">{profile?.firstName} {profile?.lastName}</p>
-          <p className="profile-tag">{profile?.tagline}</p>
+      </div>
+
+      <div className="home-feed-right">
+        <div className="home-feed-right-top">
+          <img src={ChatBubble} className="home-feed-chat-bubble" />
+          <p>{props.post.content}</p>
           <p className="profile-tag">{convertDate(props.post.createdAt)}</p>
         </div>
-      </div>
-      <p>{props.post.content}</p>
-      <div className="profile-activity-post-likes">
-        <img className="profile-activity-post-like-img" onClick={() => handleLikePostClick(props.post.id, props.viewingUser)} src={Like} />
-        <div className="profile-activity-post-like-count">{props.post.likes ? props.post.likes.length : 0}</div>
-      </div>
+
+        <div className="home-feed-right-bottom">
+          <div className="profile-feed-info-cont">
+            <p className="profile-feed-name">{profile?.firstName} {profile?.lastName}</p>
+            <p className="profile-feed-tagline">{profile?.tagline}</p>
+          </div>
+        </div>
+      </div> 
     </div>
   );
 }
