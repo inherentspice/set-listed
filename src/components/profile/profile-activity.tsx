7faf7d5e3e5e8d62f3e93817cd5fcc3a1815e2ef
Jacob Dowdle <jacobk.dowdle@gmail.com>
2023-03-03 10:55:44 +0800

    updated form style for add and edit overlays on profile page

--- src/components/profile/profile-activity.tsx
+++ src/components/profile/profile-activity.tsx
@@ -161,8 +161,8 @@ export default function ProfileActivity(props: {profileCard: ProfileCardData[], 
           <div className="expanded-profile-overlay-cont" key={profileCard.id} onClick={() => handleStartPostClose()}></div>
           <div className="expanded-profile-overlay">
             <div className="expanded-profile-overlay-header-cont">
-              <h2 className="expanded-start-post-title">Edit Your About Section</h2>
-              <img className="start-post-cancel" src={CancelButton} onClick={() => handleStartPostClose()} />
+              <h2 className="expanded-profile-overlay-header-title">Write Your Post Below</h2>
+              <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48" className="profile-overlay-header-button" onClick={() => handleStartPostClose()}><path d="m330 768 150-150 150 150 42-42-150-150 150-150-42-42-150 150-150-150-42 42 150 150-150 150 42 42Zm150 208q-82 0-155-31.5t-127.5-86Q143 804 111.5 731T80 576q0-83 31.5-156t86-127Q252 239 325 207.5T480 176q83 0 156 31.5T763 293q54 54 85.5 127T880 576q0 82-31.5 155T763 858.5q-54 54.5-127 86T480 976Zm0-60q142 0 241-99.5T820 576q0-142-99-241t-241-99q-141 0-240.5 99T140 576q0 141 99.5 240.5T480 916Zm0-340Z"/></svg>
             </div>
             <form className="start-post-user-form">
                 <div className="start-post-user-cont">
@@ -218,7 +218,7 @@ export default function ProfileActivity(props: {profileCard: ProfileCardData[], 
           <div className="expanded-profile-overlay-cont" key={selectedPost.id} onClick={() => handleEditPostClose()}></div>
           <div className="expanded-profile-overlay">
             <div className="expanded-profile-overlay-header-cont">
-              <h2 className="expanded-start-post-title">Edit Your About Section</h2>
+              <h2 className="expanded-profile-overlay-header-title">Edit Your Post Below</h2>
               <img className="start-post-cancel" src={CancelButton} onClick={() => handleEditPostClose()} />
             </div>
             <form className="start-post-user-form">
