ec7b595debcd827c890e65537a52cdec0106ed25
Jacob Dowdle <jacobk.dowdle@gmail.com>
2023-03-03 10:14:43 +0800

    updated location logic for experience

--- src/components/profile/profile-experience.tsx
+++ src/components/profile/profile-experience.tsx
@@ -324,7 +324,7 @@ export default function ProfileExperience(props: {experience: ExperienceData[], 
                   <div className="profile-experience-item-head">
                     <div className="profile-experience-head-left">
                       <div className="profile-experience-item-title">{item.title}</div>
-                      <div className="profile-experience-item-venue">{item.venue + (item.location == "undefined" ? null : " - " + item.location)}</div>
+                      <div className="profile-experience-item-venue">{item.venue + (item.location == undefined ? null : " - " + item.location)}</div>
                     </div>
                     {props.userProfile && <div className="profile-experience-item-head-right">
                       <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48" className="profile-section-body-button" onClick={() => handleEditExperienceClick(item.id)}><path d="M180 876h44l443-443-44-44-443 443v44Zm614-486L666 262l42-42q17-17 42-17t42 17l44 44q17 17 17 42t-17 42l-42 42Zm-42 42L248 936H120V808l504-504 128 128Zm-107-21-22-22 44 44-22-22Z"/></svg>
