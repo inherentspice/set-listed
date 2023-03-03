372915dbbf04886a2b48bb46cd79643dad65d682
HesitantRealism14 <rachel.liu1997@outlook.com>
2023-03-03 12:32:10 +0000

    testing new gh connection

--- src/components/error-message.tsx
+++ src/components/error-message.tsx
@@ -1,6 +1,7 @@
 import "../styles/error-message.css";
 
 export default function ErrorMessage() {
+  console.log("test")
   return (
     <div className="error-cont">
       <p>You broke something. This isn't on us. Look inwards, then try again.</p>
