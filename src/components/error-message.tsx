0cca03e626a402425a4f5b8a3ccd3fc353bb8f92
HesitantRealism14 <rachel.liu1997@outlook.com>
2023-03-03 12:39:02 +0000

    configuring permissions

--- src/components/error-message.tsx
+++ src/components/error-message.tsx
@@ -1,7 +1,6 @@
 import "../styles/error-message.css";
 
 export default function ErrorMessage() {
-  console.log("test")
   return (
     <div className="error-cont">
       <p>You broke something. This isn't on us. Look inwards, then try again.</p>
