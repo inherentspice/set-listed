8e41e058ccfe9e26e6148e26d20fb16abbee7719
Jacob Dowdle <jacobk.dowdle@gmail.com>
2023-03-04 17:18:10 +0800

    updated overlay general style

--- src/components/profile/profile-hero.tsx
+++ src/components/profile/profile-hero.tsx
@@ -221,7 +221,7 @@ export default function ProfileHero(props: {profileCard: ProfileCardData[], user
         <div className="expanded-profile-overlay-cont" key={profileCard.id} onClick={() => handleEditBackgroundClose()}></div>
         <div className="expanded-profile-overlay">
           <div className="expanded-profile-overlay-header-cont">
-            <h2 className="edit-profile-hero-title">Edit Background Image</h2>
+            <h2 className="expanded-profile-overlay-header-title">Edit Background Image</h2>
             <img className="edit-profile-hero-cancel" src={CancelButton} onClick={() => handleEditBackgroundClose()} />
           </div>
           <div className="edit-profile-hero-background">
@@ -258,7 +258,7 @@ export default function ProfileHero(props: {profileCard: ProfileCardData[], user
         <div className="expanded-profile-overlay-cont" key={profileCard.id} onClick={() => handleEditProfilePicClose()}></div>
         <div className="expanded-profile-overlay">
           <div className="expanded-profile-overlay-header-cont">
-            <h2 className="edit-profile-hero-title">Edit Profile Image</h2>
+            <h2 className="expanded-profile-overlay-header-title">Edit Profile Image</h2>
             <img className="edit-profile-hero-cancel" src={CancelButton} onClick={() => handleEditProfilePicClose()} />
           </div>
           <div className="edit-profile-hero-pic">
@@ -320,8 +320,8 @@ export default function ProfileHero(props: {profileCard: ProfileCardData[], user
         <div className="expanded-profile-overlay-cont" key={profileCard.id} onClick={() => handleEditProfileClose()}></div>
         <div className="expanded-profile-overlay">
           <div className="expanded-profile-overlay-header-cont">
-            <h1 className="edit-profile-hero-title">Edit Intro</h1>
-            <svg xmlns="http://www.w3.org/2000/svg" height="35" viewBox="0 96 960 960" width="35" className="edit-profile-cancel-button" onClick={() => handleEditProfileClose()}><path d="m330 768 150-150 150 150 42-42-150-150 150-150-42-42-150 150-150-150-42 42 150 150-150 150 42 42Zm150 208q-82 0-155-31.5t-127.5-86Q143 804 111.5 731T80 576q0-83 31.5-156t86-127Q252 239 325 207.5T480 176q83 0 156 31.5T763 293q54 54 85.5 127T880 576q0 82-31.5 155T763 858.5q-54 54.5-127 86T480 976Zm0-60q142 0 241-99.5T820 576q0-142-99-241t-241-99q-141 0-240.5 99T140 576q0 141 99.5 240.5T480 916Zm0-340Z"/></svg>
+            <h1 className="expanded-profile-overlay-header-title">Edit Intro</h1>
+            <svg xmlns="http://www.w3.org/2000/svg" height="35" viewBox="0 96 960 960" width="35" className="profile-overlay-header-button" onClick={() => handleEditProfileClose()}><path d="m330 768 150-150 150 150 42-42-150-150 150-150-42-42-150 150-150-150-42 42 150 150-150 150 42 42Zm150 208q-82 0-155-31.5t-127.5-86Q143 804 111.5 731T80 576q0-83 31.5-156t86-127Q252 239 325 207.5T480 176q83 0 156 31.5T763 293q54 54 85.5 127T880 576q0 82-31.5 155T763 858.5q-54 54.5-127 86T480 976Zm0-60q142 0 241-99.5T820 576q0-142-99-241t-241-99q-141 0-240.5 99T140 576q0 141 99.5 240.5T480 916Zm0-340Z"/></svg>
           </div>
           <form className="edit-profile-hero-form">
             <h2>Basic Information</h2>
