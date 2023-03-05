86009715b4c56a7ba28986f751c848f3e06842ae
inherentspice <stephenlehanesmith@gmail.com>
2023-03-05 20:21:40 +0000

    redesigned feed posts and added comment box

--- src/components/home/feed.tsx
+++ src/components/home/feed.tsx
@@ -11,6 +11,7 @@ import ChatBubble from "../../media/icons/chat-bubble.png";
 export default function Feed(props: {post: PostData, viewingUser: string}) {
 
   const [profile, setProfile] = useState<ProfileCardData | undefined>(undefined);
+  const [comment, setComment] = useState<string>("");
 
   useEffect(() => {
     (async function() {
@@ -45,34 +46,52 @@ export default function Feed(props: {post: PostData, viewingUser: string}) {
     }
   }
 
+  function handleCommentChange(e: React.ChangeEvent<HTMLTextAreaElement>): void{
+    setComment(e.target.value);
+  }
+
+  function handleAddCommentSubmit(e: React.MouseEvent<HTMLButtonElement, MouseEvent>, comment: string) {
+    e.preventDefault();
+
+    console.log(comment);
+  }
+
   return (
     <div className="home-feed-cont comp">
-      <div className="home-feed-left">
+      <div className="post-cont">
+        <div className="home-feed-left">
+          <a href={`/user/${props.post.user}`}>
+            {profile?.image ?
+              <img src={profile.image} alt="" className="profile-picture-medium"/>
+              : <></>}
+          </a>
+        </div>
+
+
+        <div className="home-feed-right">
+          <div className="home-feed-right-top">
+            <p>{props.post.content}</p>
+          </div>
+
+          <div className="home-feed-right-bottom">
+            <div className="profile-feed-info-cont">
+              <p className="profile-feed-name">{profile?.firstName} {profile?.lastName}</p>
+              <p className="profile-feed-tagline">{profile?.tagline}</p>
+              <p className="profile-tag">{convertDate(props.post.createdAt)}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="add-comment-cont">
         <div className="profile-activity-post-likes">
           <img className="profile-activity-post-like-img" onClick={() => handleLikePostClick(props.post.id, props.viewingUser)} src={Like} />
           <div className="profile-activity-post-like-count">{props.post.likes ? props.post.likes.length : 0}</div>
         </div>
-        <a href={`/user/${props.post.user}`}>
-          {profile?.image ?
-            <img src={profile.image} alt="" className="profile-picture-medium"/>
-            : <></>}
-        </a>
+        <form className="add-comment-form">
+          <textarea className="start-comment-area" value={comment} onChange={handleCommentChange} />
+          <button className="primary-button comment-button" type="submit" onClick={(e) => handleAddCommentSubmit(e, comment)}>Comment</button>
+        </form>
       </div>
-
-      <div className="home-feed-right">
-        <div className="home-feed-right-top">
-          <img src={ChatBubble} className="home-feed-chat-bubble" />
-          <p>{props.post.content}</p>
-        </div>
-
-        <div className="home-feed-right-bottom">
-          <div className="profile-feed-info-cont">
-            <p className="profile-feed-name">{profile?.firstName} {profile?.lastName}</p>
-            <p className="profile-feed-tagline">{profile?.tagline}</p>
-            <p className="profile-tag">{convertDate(props.post.createdAt)}</p>
-          </div>
-        </div>
-      </div> 
     </div>
   );
 }
