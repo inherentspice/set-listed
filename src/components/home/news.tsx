47738ce0d4726422cfd8e53ee405f292a00dd0da
inherentspice <stephenlehanesmith@gmail.com>
2023-03-13 10:36:17 +0000

    seperated news data into separate component

--- src/components/home/news.tsx
+++ src/components/home/news.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import convertDate from "../../utilities/convert-date";
+import newsData from "../../dummy-data/news";
 import "../../styles/home/news.css";
 
 export default function News(props: {loaded: boolean}) {
@@ -9,42 +9,6 @@ export default function News(props: {loaded: boolean}) {
       <div className="home-news-cont comp loading-news loading"></div>
     )
   }
-  interface newsDataObject {
-    title: string,
-    posted: Date,
-    readCount: number,
-  }
-
-  const newsData: newsDataObject[] = [
-    {
-      title: "What is the deal with...?",
-      posted: new Date("December 12, 2022"),
-      readCount: 3,
-    },
-    {
-      title: "Welcome to AI assisted comedy",
-      posted: new Date("January 2, 2023"),
-      readCount: 12,
-    },
-    {
-      title: "Stand-up in the meta-verse",
-      posted: new Date("December 27, 2022"),
-      readCount: 32,
-    },
-    {
-      title: "Zoom show crowdwork: study",
-      posted: new Date("December 29, 2022"),
-      readCount: 1,
-    },
-    {
-      title: "What's happening to 'woke' comedy in the UK?",
-      posted: new Date("January 3, 2023"),
-      readCount: 41
-    }
-  ];
-
-  // function to convert the date posted to the how long ago it was posted in days or hours
-
 
   return (
     <div className="home-news-cont comp">
@@ -54,7 +18,6 @@ export default function News(props: {loaded: boolean}) {
           return (
             <div className="news-list-cont">
               <li key={news.posted.toString()}>{news.title}</li>
-              {/* <p>{convertDate(news.posted)} ago | {news.readCount} readers</p> */}
             </div>
           );
         })}
